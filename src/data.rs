f55856954269ba147273d68e247f955c80139b85
szatan <szatan@lszj.org>
2025-04-01 15:59:50 +0200

    Split data points across symbols

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,9 @@
+use std::collections::HashMap;
 use serde::Serialize;
 use thiserror::Error;
 
 #[derive(Default)]
-pub struct DataPoints {}
+pub struct DataPoints(HashMap<String, DataPointsInner>);
 
 #[derive(Default, Serialize)]
 pub struct Stats {
@@ -18,15 +19,31 @@ pub struct Stats {
 pub struct InvalidParams;
 
 impl DataPoints {
-    pub fn add(&mut self, symbol: &str, values: &[f64]) {
-        println!("ADD {} {:?}", symbol, values);
+    pub fn add(&mut self, symbol: String, values: &[f64]) {
+        self.0.entry(symbol).or_default().add(values)
     }
 
     pub fn get(&self, symbol: &str, k: u8) -> Result<Stats, InvalidParams> {
+        match self.0.get(symbol) {
+            Some(dp) => dp.get(k),
+            None => Ok(Stats::default()),
+        }
+    }
+}
+
+#[derive(Default)]
+struct DataPointsInner {}
+
+impl DataPointsInner {
+    fn add(&mut self, values: &[f64]) {
+        println!("ADD {:?}", values);
+    }
+
+    fn get(&self, k: u8) -> Result<Stats, InvalidParams> {
         if k <= 0 || k > 8 {
             return Err(InvalidParams);
         }
-        println!("GET {} {}", symbol, k);
+        println!("GET {}", k);
         Ok(Stats::default())
     }
 }
